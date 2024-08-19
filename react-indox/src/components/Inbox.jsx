30dfc0fa36516878e76e4245b10f46619ab1bdde
Priyadharshini S <priyadharshinis5102@gmail.com>
2024-08-19 14:04:58 +0530

    Updated

--- react-indox/src/components/Inbox.jsx
+++ react-indox/src/components/Inbox.jsx
@@ -3,27 +3,7 @@ import { useSelector } from "react-redux";
 import {MailBox,NotFound,LeftSection,RightSection} from './index.js'
 function Inbox() {
   const currentMail = useSelector((state) => state.mails.mails);
-  // const [selectedMail, setSelectedMail] = useState([]);
-  // useEffect(() => {
-  //   const fetch = async () => {
-  //     try {
-  //       const token = localStorage.getItem("token");
-  //       const res = await axios.get(
-  //         `https://hiring.reachinbox.xyz/api/v1/onebox/messages/${currentMail.threadId}`,
-  //         {
-  //           headers: {
-  //             Authorization: token,
-  //           },
-  //         }
-  //       );
-  //       setSelectedMail(res.data.data);
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   };
-  //   fetch();
-  // }, [currentMail]);
-
+  
   return (
     <div className="flex justify-between">
       <LeftSection></LeftSection>
